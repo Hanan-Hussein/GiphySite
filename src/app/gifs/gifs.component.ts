74acdae64b7cffa9dc422db74c1cef434a29b49c
Hanan-Hussein <hanhuss035@gmail.com>
2022-04-17 17:34:58 +0300

    ft:trendingPosts-added trending gifs

--- src/app/gifs/gifs.component.ts
+++ src/app/gifs/gifs.component.ts
@@ -8,26 +8,38 @@ import { DataService } from '../data.service';
 })
 export class GifsComponent implements OnInit {
   title = 'gifySite';
-  link="https://api.giphy.com/v1/gifs/search?api_key=5ZVOk6wXmIMBpLgT0EH7U8AHhnscJRuU&q=";
-  gifs:any=[];
-  constructor( private dataService: DataService) { }
+  link = "https://api.giphy.com/v1/gifs/search?api_key=5ZVOk6wXmIMBpLgT0EH7U8AHhnscJRuU&limit=20&q=";
+  gifs: any = [];
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
-  }
-    
-  performSearch(searchTerm: HTMLInputElement){
-    console.log(searchTerm.value);
-    
-    console.log("Button CLick is working");
-    this.dataService.getImage(this.link+ searchTerm.value).subscribe((res:any)=>{
-      if(res){
+    this.dataService.getImage("https://api.giphy.com/v1/gifs/trending?api_key=5ZVOk6wXmIMBpLgT0EH7U8AHhnscJRuU").subscribe((res: any) => {
+      if (res){
+        console.log(res);
         this.gifs.push(res);
-        this.gifs=this.gifs[0].data        
-        
-        
+        this.gifs = this.gifs[0].data
 
       }
     })
+
+  }
+
+  performSearch(searchTerm: any, loginForm: any) {
+    this.gifs=[];
+    console.log(searchTerm.value);
+
+    console.log("Button CLick is working");
+    this.dataService.getImage(this.link + searchTerm.value).subscribe((res: any) => {
+      if (res) {
+        console.log(res);
+        
+        this.gifs.push(res);
+        this.gifs = this.gifs[0].data
+
+      }
+    });
+    loginForm.reset();
+
   }
 
 }
